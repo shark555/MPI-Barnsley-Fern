4fa2acf143c74412433190401c38130da4290aa0
d.plawecki <d.plawecki@netigen.pl>
2013-06-07 02:35:14 +0200

    Rysowanie numeru klatki (SDL_ttf)

--- drawer.cpp
+++ drawer.cpp
@@ -11,19 +11,30 @@ Drawer::Drawer(int width,int height,int r_v,int g_v,int b_v) {
         fprintf(stderr, "Unable to set %dx%d video: %s\n",width,height,SDL_GetError());
         exit(1);
     }
-    surface = SDL_CreateRGBSurface(SDL_HWSURFACE, 1024, 768, 16,NULL, NULL, NULL, NULL); 
+    TTF_Init();
+    font = TTF_OpenFont( "Vera.ttf", 15 );   
+    surface = SDL_CreateRGBSurface(SDL_HWSURFACE, width, height, 16,NULL, NULL, NULL, NULL); 
     this->r = r_v;
     this->g = g_v;
-    this->b = b_v;    
+    this->b = b_v;   
+
 }
 
-int Drawer::drawFrame(int* xframe,int* yframe,int size) {        
+Drawer::~Drawer() {
+        SDL_FreeSurface( message );
+        TTF_CloseFont( font ); 
+        TTF_Quit();
+        SDL_Quit();
+}
+
+int Drawer::drawFrame(int* xframe,int* yframe,int size,int frame_id) {           
         for(int i=0;i<size;i++) {
                 drawPixel(r,g,b,xframe[i],yframe[i]);
         }
         SDL_BlitSurface(surface, NULL, screen,NULL);   
         SDL_Flip(screen);        
         SDL_FillRect(surface, NULL, 0);
+        drawFrameId(frame_id);
         return 0;
 }
 
@@ -45,3 +56,19 @@ int Drawer::drawPixel(Uint8 R, Uint8 G, Uint8 B, int x,int y)
 
     return 0;
 }
+
+void Drawer::apply_surface( int x, int y, SDL_Surface* source, SDL_Surface* destination)
+{
+    SDL_Rect offset;
+    offset.x = x;
+    offset.y = y;
+    SDL_BlitSurface( source, NULL, destination, &offset );
+}
+
+void Drawer::drawFrameId(int frame_id) {
+     char* text  = (char*)malloc(10*sizeof(char)); 
+     sprintf(text,"%d",frame_id);
+     message = TTF_RenderText_Solid(font,text ,{255,255,255});  
+     apply_surface( 5, 5, message, surface );     
+}
+
--- drawer.h
+++ drawer.h
@@ -1,15 +1,20 @@
 #ifndef DRAWER_H
 #define	DRAWER_H
 #include <SDL.h>
+#include "SDL/SDL_ttf.h"
 
 class Drawer {
 public:
     Drawer(int width,int height,int r,int g,int b);
-    int drawFrame(int* xframe,int* yframe,int size);
+    virtual ~Drawer();
+    int drawFrame(int* xframe,int* yframe,int size,int frame_id);
 private:
     int r,g,b;
-    SDL_Surface *screen,*surface;
+    SDL_Surface *screen,*surface,*message;
+    TTF_Font *font = NULL;
     int drawPixel(Uint8 R, Uint8 G, Uint8 B, int x,int y);
+    void apply_surface( int x, int y, SDL_Surface* source, SDL_Surface* destination);
+    void drawFrameId(int frame_id);
 };
 
 #endif	/* DRAWER_H */
